b8996487f189c1b3593384523e3c4847f9b12fdf
Izaías Lima <izaiaslima356@gmail.com>
2023-01-25 23:08:50 -0300

    chore: restructuring interface

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,24 +1,22 @@
-import { ReactNode } from 'react';
-import { Header } from './Header';
-import { Sidebar } from './sidebar/Sidebar';
+import { ReactNode } from "react";
+import { Header } from "./Header";
+import { Sidebar } from "./sidebar/Sidebar";
 
 interface ILayout {
-  children?: ReactNode;
+	children?: ReactNode;
 }
 
 export function Layout({ children }: ILayout) {
-  return (
-    <div className="flex h-full w-full bg-[#212B36]">
-      <Sidebar />
-      <div className="flex flex-col px-4 pt-4 pb-16 w-full rounded-tl-3xl bg-[#FDFDFD]">
-        <Header />
-        <div
-          className="flex flex-col w-full my-0 mx-auto max-w-[1500px]
+	return (
+		<div className="flex h-full w-full bg-[#212B36]">
+			<div className="flex flex-col px-4 pt-4 pb-16 w-full rounded-tl-3xl bg-[#FDFDFD]">
+				<div
+					className="flex flex-col w-full my-0 mx-auto max-w-[1500px]
 				mt-8"
-        >
-          {children}
-        </div>
-      </div>
-    </div>
-  );
+				>
+					{children}
+				</div>
+			</div>
+		</div>
+	);
 }
